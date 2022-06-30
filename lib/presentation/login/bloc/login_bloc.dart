4c72cb6dd49883be476260614463b266e491a76c
Ilia Kurtov <kurtovi@gmail.com>
2022-06-30 21:33:33 +0300

    Added bloc widgets on login page

--- lib/presentation/login/bloc/login_bloc.dart
+++ lib/presentation/login/bloc/login_bloc.dart
@@ -2,14 +2,32 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
 
 part 'login_event.dart';
 part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
   LoginBloc() : super(LoginState.initial()) {
-    on<LoginLoginButtonClicked>((event, emit) {
-      emit(state.copyWith(authenticated: true));
-    });
+    on<LoginLoginButtonClicked>(_loginButtonClicked);
+  }
+
+  FutureOr<void> _loginButtonClicked(
+    LoginLoginButtonClicked event,
+    Emitter<LoginState> emit,
+  ) {
+    emit(state.copyWith(authenticated: true));
+  }
+
+  @override
+  void onEvent(LoginEvent event) {
+    debugPrint('Login Bloc. Event happened: $event');
+    super.onEvent(event);
+  }
+
+  @override
+  void onTransition(Transition<LoginEvent, LoginState> transition) {
+    debugPrint('Login Bloc. Transition happened: $transition');
+    super.onTransition(transition);
   }
 }
