9466d89d3ca9e39312c0cc4c807885260178e451
Ilia Kurtov <kurtovi@gmail.com>
2022-06-30 21:23:51 +0300

    Login bloc state

--- lib/presentation/login/bloc/login_bloc.dart
+++ lib/presentation/login/bloc/login_bloc.dart
@@ -7,9 +7,9 @@ part 'login_event.dart';
 part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
-  LoginBloc() : super(LoginInitial()) {
-    on<LoginEvent>((event, emit) {
-      // TODO: implement event handler
+  LoginBloc() : super(LoginState.initial()) {
+    on<LoginLoginButtonClicked>((event, emit) {
+      emit(state.copyWith(authenticated: true));
     });
   }
 }
