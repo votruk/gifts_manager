2c970f6adfee2fb24aa53b29b7042c5677308cb5
Ilia Kurtov <ilia.kurtov@fudy.eu>
2022-07-21 20:30:03 +0300

    Registration states

--- lib/presentation/registration/bloc/registration_bloc.dart
+++ lib/presentation/registration/bloc/registration_bloc.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:gift_manager/data/model/request_error.dart';
+import 'package:gift_manager/presentation/registration/model/errors.dart';
 
 part 'registration_event.dart';
 part 'registration_state.dart';
