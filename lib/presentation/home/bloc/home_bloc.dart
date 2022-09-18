75025b4a2be00b0e9a800730dd665a9522418cee
Ilia Kurtov <ilia.kurtov@fudy.eu>
2022-09-18 20:36:03 +0200

    Added logout interactor

--- lib/presentation/home/bloc/home_bloc.dart
+++ lib/presentation/home/bloc/home_bloc.dart
@@ -4,6 +4,7 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:gift_manager/data/http/model/user_dto.dart';
 import 'package:gift_manager/data/repository/user_repository.dart';
+import 'package:gift_manager/domain/logout_interactor.dart';
 
 part 'home_event.dart';
 
@@ -11,12 +12,22 @@ part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   final UserRepository userRepository;
+  final LogoutInteractor logoutInteractor;
+
+  late final StreamSubscription _logoutSubscription;
 
   HomeBloc({
     required this.userRepository,
+    required this.logoutInteractor,
   }) : super(HomeInitial()) {
     on<HomePageLoaded>(_onHomePageLoaded);
     on<HomeLogoutPushed>(_onLogoutPushed);
+    on<HomeExternalLogout>(_onHomeExternalLogout);
+    _logoutSubscription = userRepository
+        .observeItem()
+        .where((user) => user == null)
+        .take(1)
+        .listen((_) => _logout());
   }
 
   FutureOr<void> _onHomePageLoaded(
@@ -31,10 +42,28 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
     emit(HomeWithUser(user));
   }
 
+  FutureOr<void> _onHomeExternalLogout(
+    final HomeExternalLogout event,
+    final Emitter<HomeState> emit,
+  ) async {
+    emit(const HomeGoToLogin());
+  }
+
   FutureOr<void> _onLogoutPushed(
     final HomeLogoutPushed event,
     final Emitter<HomeState> emit,
-  ) async {}
+  ) async {
+    await logoutInteractor.logout();
+    _logout();
+  }
 
-  void _logout() {}
+  void _logout() {
+    add(const HomeExternalLogout());
+  }
+
+  @override
+  Future<void> close() {
+    _logoutSubscription.cancel();
+    return super.close();
+  }
 }
